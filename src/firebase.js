d2ba14f308b67a060f2185271352bc9e50b3ee4c
Md Aashiyan Ali Ansari <aashiyan.shaan@gmail.com>
2026-09-19 21:13:21 +0530

    fix firebase - remove anonymous auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,20 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInAnonymously } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBcWC_s3msiUe97eSJ8Oq3KJn1DKB0Zlng",
-  authDomain: "sajdah-planner-d840b.firebaseapp.com",
-  projectId: "sajdah-planner-d840b",
-  storageBucket: "sajdah-planner-d840b.firebasestorage.app",
-  messagingSenderId: "269604780518",
-  appId: "1:269604780518:web:38704fd27ac70f7f233e27",
-  measurementId: "G-H74TD0VCKE",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
-export { signInAnonymously };
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
 export const storage = getStorage(app);
